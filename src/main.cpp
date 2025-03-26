b2d7889cc16ce71fae2f68139e85550c75770ad9
ZedKay <zedkay2011@gmail.com>
2025-03-26 17:31:13 +0100

    refactor code

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,20 @@
 #include <vector>
 #include <algorithm>
 
+enum CommandType {
+	quit,
+	echo,
+	type,
+	invalid,
+};
+
+CommandType commandToType(std::string& command) {
+	if (command == "exit") return quit;
+	if (command == "echo") return echo;
+	if (command == "type") return type;
+	return invalid;
+}
+
 int main() {
   // Flush after every std::cout / std:cerr
   std::cout << std::unitbuf;
@@ -12,52 +26,52 @@ int main() {
   while (true) {
 	  std::cout << "$ ";
 
-	  std::string command{};
-	  std::getline(std::cin, command);
+	  std::string input{};
+	  std::getline(std::cin, input);
 
 	  // set the recognized built-in commands
 	  std::vector<std::string> builtins{ "exit", "echo", "type" };
 
-	  // Get the built-in command
-	  std::string token{ command.substr(0, command.find(" ")) };
+	  // Get the built-in command and the provided arguments
+	  std::string command{ input.substr(0, input.find(" ")) };
+	  std::string argument{ input.substr(input.find(" ") + 1 , input.size()) };
 
-	  // Handle the token
-	  // -- exit built-in command
-	  if (token == "exit")
+	  // Handle the command
+	  switch (commandToType(command))
+	  {
+	  case quit: // -- exit built-in command
 		  return 0;
 
-	  // -- echo built-in command
-	  else if (token == "echo") {
+	  case echo: // -- echo built-in command
 		  // If there are no arguments, only the <newline> is written.
-		  if (command.find(" ") >= command.size()) {
+		  if (input.find(" ") >= input.size()) {
 			  std::cout << '\n';
 			  continue;
 		  }
 
 		  // Write the arguments to standard output, followed by a <newline>
-		  std::string argument{ command.substr(command.find(" ") + 1 , command.size()) };
 		  std::cout << argument << "\n";
-	  }
+		  break;
 
-	  // -- type built-in command
-	  else if (token == "type") {
+	  case type: // -- type built-in command
 		  // If there are no arguments, user is reminded of the command syntax.
-		  if (command.find(" ") >= command.size()) {
+		  if (input.find(" ") >= input.size()) {
 			  std::cout << "Missing argument: type <command>\n";
 			  continue;
 		  }
 
 		  // Determine how the command (argument) would be interpreted if used.
-		  std::string argument{ command.substr(command.find(" ") + 1 , command.size()) };
 		  std::cout << argument;
 		  if (!std::count(builtins.begin(), builtins.end(), argument)) // unrecognized command
 			  std::cout << ": not found\n";
 		  else // built-in command
 			  std::cout << " is a shell builtin\n";
+
+		  break;
+
+	  case invalid: // -- Unrecognized command
+		  std::cout << command << ": command not found\n";
 	  }
 
-	  // -- Unrecognized command
-	  else
-		  std::cout << command << ": command not found\n";
   }
 }
